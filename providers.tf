a2b06d248cd56d402cf3719da47d2631fc310b7f
singhsanjeev632 <singhsanjeev632@gmail.com>
2025-11-04 11:03:43 +0000

    updates

---
+++ providers.tf
@@ -0,0 +1,29 @@
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+ #region_location_info
+ region = "ap-southeast-2"
+}
+
+# creating ec2
+resource "aws_instance" "example" {
+   ami = "ami-0a25a306450a2cba3"
+   instance_type  = "t2.nano"
+   key_name = "roche-key" 
+   tags_all = {
+     "Name" = "sanjeev-vm-1"
+   }
+   
+   
+}
